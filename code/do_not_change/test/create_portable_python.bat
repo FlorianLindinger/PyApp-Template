517ad90430371e50827f192728f2736ff93ff456
Florian.Lindinger <florianlindinger26@gmail.com>
2025-10-21 17:55:53 +0200

    WIP for general input

--- code/do_not_change/test/create_portable_python.bat
+++ code/do_not_change/test/create_portable_python.bat
@@ -1,20 +1,44 @@
+:: WIP: make arg version and make latest default for no arg and test all possibitlies
+:: check if it upgrades to 10+ for 9 version
+:: check if more final files deletable
+:: check if fully portable
+
 @echo off
 setlocal EnableExtensions EnableDelayedExpansion
 
-set "PY_VER=3.13"
-
-:: WIP: make arg version and make latest default for no arg and test all possibitlies
-:: check if it upgrades to 10+ for 9 version
-
 :: find latest python version compatible with PY_VER to find download link
+@REM for /f "usebackq delims=" %%A in (`
+@REM   powershell -NoLogo -NoProfile -Command "$v=(Invoke-WebRequest 'https://www.python.org/ftp/python/').Links.href | Where-Object {$_ -match '^%PY_VER%\.\d+/$'} | Sort-Object | Select-Object -Last 1; if($v){$v.TrimEnd('/')}" 
+@REM `) do set "FULL_VER=%%A"
+
+set "PY_VER=%~1"
+
 for /f "usebackq delims=" %%A in (`
-  powershell -NoLogo -NoProfile -Command "$v=(Invoke-WebRequest 'https://www.python.org/ftp/python/').Links.href | Where-Object {$_ -match '^%PY_VER%\.\d+/$'} | Sort-Object | Select-Object -Last 1; if($v){$v.TrimEnd('/')}" 
+  powershell -NoLogo -NoProfile -Command ^
+    "$minor='%PY_VER%';" ^
+    "$pattern = ('^' + [regex]::Escape($minor) + '\\.\\d+/$');" ^
+    "$all = (Invoke-WebRequest 'https://www.python.org/ftp/python/' -UseBasicParsing).Links |" ^
+    "  Where-Object href -match $pattern |" ^
+    "  ForEach-Object href | ForEach-Object { $_.TrimEnd('/') } |" ^
+    "  Sort-Object {[version]$_} -Descending;" ^
+    "foreach($v in $all) {" ^
+    "  $url = 'https://www.python.org/ftp/python/' + $v + '/amd64/';" ^
+    "  try { (Invoke-WebRequest -Method Head -Uri $url -UseBasicParsing -ErrorAction Stop) > $null; Write-Output $v; exit 0 } catch {}" ^
+    "}" ^
+    "exit 1"
 `) do set "FULL_VER=%%A"
+
+
+:: abort if fail
+ECHO: (%FULL_VER%)
 if not defined FULL_VER (
-    echo [ERROR] Could not determine latest patch for %PY_VER%.
+    echo [ERROR] Could not determine latest version for specified in%PY_VER%. Aborting. Press any key to exit.
+    PAUSE > NUL
     exit /b 1
 )
+:: define URL based on full version
 set "URL=https://www.python.org/ftp/python/%FULL_VER%/amd64/"
+ECHO: Python version: %FULL_VER%
 ECHO: Download URL: %URL%
 
 :: (re)create tmp file
