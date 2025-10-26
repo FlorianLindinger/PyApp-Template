afa55c0d2eb04ed0c9eca225621020c85c4fb5b0
Florian.Lindinger <florianlindinger26@gmail.com>
2025-10-26 17:22:53 +0100

    renamed folders and removed included python. Added files to create portable python and venv; updated gitignore for hcanges

--- code/do_not_change/start_program.bat
+++ code/do_not_change/start_program.bat
@@ -1,30 +1,30 @@
-@REM ########################
-@REM --- Code Description ---
-@REM ########################
+:: ========================
+:: --- Code Description ---
+:: ========================
 
-@REM #########################
-@REM --- Setup & Variables ---
-@REM #########################
+:: =========================
+:: --- Setup & Variables ---
+:: =========================
 
-@REM turn off printing of commands:
+:: turn off printing of commands:
 @ECHO OFF
 
-@REM make this code local so no variables of a potential calling program are changed:
+:: make this code local so no variables of a potential calling program are changed:
 SETLOCAL
 
-@REM define local variables (do not have spaces before or after the "=" or at the end of the variable value (unless wanted in value) -> inline comments without space before "&@REM".
-@REM Use "\" to separate folder levels and omit "\" at the end of paths. Relative paths allowed):
+:: define local variables (do not have spaces before or after the "=" or at the end of the variable value (unless wanted in value) -> inline comments without space before "&@REM".
+:: Use "\" to separate folder levels and omit "\" at the end of paths. Relative paths allowed):
 SET "settings_path=%~1"
 
-@REM move to folder of this file (needed for relative path shortcuts)
-@REM current_file_path varaible needed as workaround for nieche windows bug where this file gets called with quotation marks:
+:: move to folder of this file (needed for relative path shortcuts)
+:: current_file_path varaible needed as workaround for nieche windows bug where this file gets called with quotation marks:
 SET "current_file_path=%~dp0"
 CD /D "%current_file_path%"
 
-@REM import settings from settings_path:
+:: import settings from settings_path:
 FOR /F "tokens=1,2 delims==" %%A IN ('findstr "^" "%settings_path%"') DO ( SET "%%A=%%B" )
 
-@REM convert the path settings that are relative to settings file to absolute paths:
+:: convert the path settings that are relative to settings file to absolute paths:
 FOR %%I IN ("%settings_path%") DO SET "settings_dir=%%~dpI"
 CD /D "%settings_dir%"
 CALL :MAKE_ABSOLUTE_PATH_IF_RELATIVE "%icon_path%" 
@@ -37,23 +37,28 @@ CALL :MAKE_ABSOLUTE_PATH_IF_RELATIVE "%after_python_crash_code_path%"
 SET "after_python_crash_code_path=%OUTPUT%"
 CD /D "%current_file_path%"
 
-@REM ######################
-@REM --- Code Execution ---
-@REM ######################
+:: ======================
+:: --- Code Execution ---
+:: ======================
 
-@REM change terminal title:
+:: change terminal title:
 TITLE %program_name%
 
-@REM change terminal colors (for starting lines):
+:: change terminal colors (for starting lines):
 COLOR %terminal_bg_color%%terminal_text_color%
 
-@REM change terminal icon:
+:: change terminal icon:
 change_icon "%program_name%" "%icon_path%"
 
-@REM activate or create & activate python environment:
+:: activate or create & activate python environment:
 CALL "%python_env_activation_code_path%" "nopause"
+IF %ERRORLEVEL% NEQ 0 (
+	ECHO: [Error] Environment activation failed (see above). Aborting. Press any key to exit.
+	PAUSE >NUL 
+	EXIT /B 1
+)
 
-@REM normalize python code paths to full absolute path and get its directory paths:
+:: normalize python code paths to full absolute path and get its directory paths:
 FOR %%F IN ("%python_code_path%") DO (
     SET "abs_python_code_path=%%~fF"
     SET "python_code_dir=%%~dpF"
@@ -63,96 +68,96 @@ FOR %%F IN ("%after_python_crash_code_path%") DO (
     SET "crash_python_code_dir=%%~dpF"
 )
 
-@REM run main python code:
-@REM go to directory of python code and execute it and return to folder of this file:
+:: run main python code:
+:: go to directory of python code and execute it and return to folder of this file:
 CD /D "%python_code_dir%"
 python "%abs_python_code_path%"
 CD /D "%current_file_path%"
 
-@REM %ERRORLEVEL% is what the last python execution gives out in sys.exit(errorlevel). 
-@REM Errorlevel 1 (default for python crash) will run main_code.py or after_python_crash_code.py (depending on parameter restart_main_code_on_crash in non-user_settings.ini). Errorlevel -1 will exit the terminal. Any other value will pause the terminal until user presses a button (unless this script is called with any argument):
+:: %ERRORLEVEL% is what the last python execution gives out in sys.exit(errorlevel). 
+:: Errorlevel 1 (default for python crash) will run main_code.py or after_python_crash_code.py (depending on parameter restart_main_code_on_crash in non-user_settings.ini). Errorlevel -1 will exit the terminal. Any other value will pause the terminal until user presses a button (unless this script is called with any argument):
 IF %ERRORLEVEL% EQU 1 (
 	SET original_python_crashed=1
 	CALL :handle_python_crash
 )
-@REM Does not pause if python returns an errorlevel -1 with sys.exit(-1) in python:
+:: Does not pause if python returns an errorlevel -1 with sys.exit(-1) in python:
 IF %ERRORLEVEL% EQU -1 (
 	EXIT /B
 )
 
-@REM print final report message:
+:: print final report message:
 ECHO:
 IF "%original_python_crashed%"=="1" (
 	IF "%python_crash_handler_crashed%"=="1" (
-		ECHO: ########################################################
+		ECHO: ========================================================
 		ECHO: Finished all python execution.
 		ECHO: The main python code crashed and the python function for
 		ECHO: handling crashes crashed at least once before finishing 
 		ECHO: successfully now (see above^)
-		ECHO: ########################################################
+		ECHO: ========================================================
 	) ELSE (
-		ECHO: ######################################################
+		ECHO: ======================================================
 		ECHO: Finished all python execution.
 		ECHO: The main python code crashed but the python function
 		ECHO: for handling crashes finished successfully (see above^)
-		ECHO: ######################################################
+		ECHO: ======================================================
 	)
 ) ELSE (
-	ECHO: #################################
+	ECHO: =================================
 	ECHO: Python code finished successfully
-	ECHO: #################################
+	ECHO: =================================
 )
 ECHO:
 
-@REM ####################
-@REM --- Closing-Code ---
-@REM ####################
+:: ====================
+:: --- Closing-Code ---
+:: ====================
 
-@REM pause if not called by other script with multiple arguments:
+:: pause if not called by other script with multiple arguments:
 IF "%~2"=="" (
 	ECHO: Press any key to exit
 	PAUSE >NUL 
 )
 
-@REM exit program without closing a potential calling program
+:: exit program without closing a potential calling program
 EXIT /B
 
-@REM ############################
-@REM --- Function Definitions ---
-@REM ############################
+:: ============================
+:: --- Function Definitions ---
+:: ============================
 
-@REM -------------------------------------------------
-@REM define handle_python_crash function:
-@REM -------------------------------------------------
+:: -------------------------------------------------
+:: define handle_python_crash function:
+:: -------------------------------------------------
 :handle_python_crash
 ECHO:
-ECHO: ###################################################
+ECHO: ===================================================
 ECHO: WARNING: Python returned 1, which indicates a crash
-ECHO: ###################################################
+ECHO: ===================================================
 ECHO:
 IF %restart_main_code_on_crash% EQU 0 ( @REM  run after_python_crash_code.py (again)
 	IF EXIST "%after_python_crash_code_path%" (
 		ECHO:
-		ECHO: ###############################################
+		ECHO: ===============================================
 		ECHO: Running python code intended for after crashes:
-		ECHO: ###############################################
+		ECHO: ===============================================
 		ECHO:
-		@REM go to directory of python code and execute it and return to folder of this file:	
+		:: go to directory of python code and execute it and return to folder of this file:	
 		CD /D "%python_code_dir%"
 		python "%abs_python_code_path%"
 		CD /D "%current_file_path%"
 		ECHO:
-	@REM exit function if after_python_crash_code does not exist
+	:: exit function if after_python_crash_code does not exist
 	) ELSE (
 		EXIT /B 0 &@REM exit function with errorcode 0
 	)
 )	ELSE (  @REM run main_code.py again
 	ECHO:
-	ECHO: ################################################
+	ECHO: ================================================
 	ECHO: Running main python code again after it crashed:
-	ECHO: ################################################
+	ECHO: ================================================
 	ECHO:
-	@REM go to directory of python code and execute it and return to folder of this file:
+	:: go to directory of python code and execute it and return to folder of this file:
 	CD /D "%crash_python_code_dir%"
 	python "%abs_crash_python_code_path%" "crashed" &@REM argument "crashed" indicated to the python code that it is a repeat call after a crash and can be checked for with sys.argv[-1]=="crashed"
 	CD /D "%current_file_path%"
@@ -163,12 +168,12 @@ IF %ERRORLEVEL% EQU 1 ( @REM could be infinitely recursive
 	CALL :handle_python_crash
 )
 EXIT /B 0 &@REM exit function with errorcode 0
-@REM -------------------------------------------------
+:: -------------------------------------------------
 
-@REM -------------------------------------------------
-@REM function that makes relative path (relative to current working directory) to absolute if not already:
-@REM -------------------------------------------------
+:: -------------------------------------------------
+:: function that makes relative path (relative to current working directory) to absolute if not already:
+:: -------------------------------------------------
 :make_absolute_path_if_relative
 	SET "OUTPUT=%~f1"
 	GOTO :EOF
-@REM -------------------------------------------------
+:: -------------------------------------------------
