52a8fa267fad1d4b8a661850e1320e7f063cc512
Florian.Lindinger <florianlindinger26@gmail.com>
2025-11-03 21:27:51 +0100

    removed need for helper file; update commetns; cleaned files

--- code/do_not_change/utilities/batch_file/run_batch_with_file_output.bat
+++ code/do_not_change/utilities/batch_file/run_batch_with_file_output.bat
@@ -1,24 +1,15 @@
-@REM ########################
-@REM --- Code Description ---
-@REM ########################
+:: ========================
+:: --- Code Description ---
+:: ========================
 
-@REM #########################
-@REM --- Setup & Variables ---
-@REM #########################
+:: =========================
+:: --- Setup & Variables ---
+:: =========================
 
-@REM turn off printing of commands:
-@ECHO OFF
+:: turn off printing of commands and use local variables
+@ECHO OFF & SETLOCAL enabledelayedexpansion
 
-@REM make this code local so no variables of a potential calling program are changed:
-SETLOCAL enabledelayedexpansion
-
-@REM move to folder of this file (needed for relative path shortcuts)
-@REM current_file_path varaible needed as workaround for nieche windows bug where this file gets called with quotation marks:
-rem SET "current_file_path=%~dp0"
-rem CD /D "%current_file_path%"
-
-@REM define local variables (do not have spaces before or after the "=" or at the end of the variable value (unless wanted in value) -> inline comments without space before "&@REM".
-@REM Use "\" to separate folder levels and omit "\" at the end of paths. Relative paths allowed):
+:: set variables from args or default args:
 SET "batch_file_path=%~1"
 IF "%~2"=="" (
 	SET "log_path=log.txt"
@@ -26,16 +17,16 @@ IF "%~2"=="" (
 	SET "log_path=%~2"
 )
 
-@REM ######################
-@REM --- Code Execution ---
-@REM ######################
+:: ======================
+:: --- Code Execution ---
+:: ======================
 
-@REM makes python files (if called) flush immediately what they print to the log file
+:: makes python files (if called) flush immediately what they print to the log file
 SET "PYTHONUNBUFFERED=1"
-@REM utf-8 encoding needed for python output (if called) to avoid errors for special characters
+:: utf-8 encoding needed for python output (if called) to avoid errors for special characters
 SET "PYTHONIOENCODING=utf-8"
 
-@REM put arguments starting from the third (from calling this batch file) in the string "args_list" with space in between and each surrouned by " on both sides:
+:: put arguments starting from the third (from calling this batch file) in the string "args_list" with space in between and each surrouned by " on both sides:
 SET "args_list="
 SET "i=3"
 :loop_args
@@ -47,9 +38,9 @@ SET "i=3"
 GOTO loop_args
 :args_done
 
-@REM run batch file and redirect print and error output to log_path
+:: run batch file and redirect print and error output to log_path
 CALL "%batch_file_path%" %args_list% > "%log_path%" 2>&1
 
-@REM exit program without closing a potential calling program
+:: exit program without closing a potential calling program
 EXIT /B 0
 
