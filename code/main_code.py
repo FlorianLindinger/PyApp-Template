a454a6f0f932816b4c24da394603d2cff8ea4212
Florian.Lindinger <florianlindinger26@gmail.com>
2025-11-11 22:26:44 +0100

    remove temp prints for test

--- code/main_code.py
+++ code/main_code.py
@@ -4,10 +4,3 @@
 # Optional: Imports and converts user variables (e.g., name: value) in settings.yaml (access value via dictionary: s["name"]). Alternatively use settings.py directly.
 from settings import s # <-needs pyyaml package # noqa isort: skip type: ignore fmt: off
 # ==============================================================================
-
-
-import time
-
-for i in range(50):
-    print(i)
-    time.sleep(0.1)
