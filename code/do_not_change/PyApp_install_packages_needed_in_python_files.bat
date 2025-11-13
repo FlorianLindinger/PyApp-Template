669fee6bc08512694558471eeb8a47a385ec3227
Florian.Lindinger <florianlindinger26@gmail.com>
2025-11-13 18:43:05 +0100

    fixed install packages needed in python files for new code approach. made it into no utility; update its shortcut;update todo;

---
+++ code/do_not_change/PyApp_install_packages_needed_in_python_files.bat
@@ -0,0 +1,54 @@
+:: ========================
+:: --- Code Description ---
+:: ========================
+
+
+
+
+
+:: ====================================
+:: --- Setup, Variables, and Checks ---
+:: ====================================
+
+:: turn off printing of commands &  make variables local:
+@echo off & setlocal
+
+:: move to folder of this file (needed for relative path shortcuts)
+:: current_file_path varaible needed as workaround for nieche windows bug where this file gets called with quotation marks:
+set "current_file_path=%~dp0"
+cd /d "%current_file_path%"
+
+:: define local variables (with relative paths being relative to this file)
+set "environment_activator_path=PyApp_install_and_or_activate_python_env.bat"
+set "folder_of_python_files_to_search=..\"
+set "python_file_requirements_path=..\python_environment\python_file_requirements.txt"
+set "python_environment_path=..\python_environment"
+
+:: ======================
+:: --- Code Execution ---
+:: ======================
+
+:: activate (or create & activate) python environment:
+call "%environment_activator_path%"
+
+python -m venv tmp_env_for_install_package_from_files
+tmp_env_for_install_package_from_files/Scripts/activate   
+pip install pipreqs --disable-pip-version-check > nul
+pipreqs "%folder_of_python_files_to_search%" --force --savepath "%python_file_requirements_path%" --ignore "%python_environment_path%"?
+deactivate
+:: carefull with name because it will delete everything:
+rmdir /s /q tmp_env_for_install_package_from_files
+
+:: reactivate python environment:
+call "%environment_activator_path%"
+
+:: install list of needed packages
+pip install --disable-pip-version-check --upgrade -r "%python_file_requirements_path%"
+
+:: print final message and exit after key press
+echo.
+echo.
+echo.
+echo Everything needed in the python files should be installed now if no errors are above. Press any key to exit.
+pause > nul
+exit 0
