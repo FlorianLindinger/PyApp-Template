d0b4d944109e633bd322f1eea1d9396919d92af0
Florian.Lindinger <florianlindinger26@gmail.com>
2025-11-19 21:40:18 +0100

    WIP test for tkinter terminal; not working x

---
+++ code/do_not_change/general_utilities/test.py
@@ -0,0 +1,12 @@
+while True:
+    msg=input()
+    print(msg)
+
+
+
+# ideas:
+
+
+# always ontop option or on/off Button
+# highlight on print option or on/off Button
+# confimration proomtp on close or no allow close
