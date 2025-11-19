97b5791e6c05bc72fd8a009f442d99c29a528851
Florian.Lindinger <florianlindinger26@gmail.com>
2025-11-19 23:24:57 +0100

    minimize to tray button now has blue hover; update todo

--- code/do_not_change/general_utilities/test.py
+++ code/do_not_change/general_utilities/test.py
@@ -1,5 +1,8 @@
+import time
+
 while True:
     msg=input()
+    time.sleep(3)
     print(msg)
 
 
@@ -13,3 +16,11 @@ while True:
 # highlight on print option or on/off Button on left
 # confimration proomtp on close or no allow close
 # option to turn on/off printing of commands and button on left
+# open soruce code on click on title
+# change button icons
+# blue color on minimize to tray button
+# make it stay alert on print until seen
+# make all button color on 
+
+
+
