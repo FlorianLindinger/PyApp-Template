8e4193ce76aa3ba18b737c53150794eda2998faa
Florian.Lindinger <florianlindinger26@gmail.com>
2025-11-19 21:45:15 +0100

    working x button; update todo

--- code/do_not_change/general_utilities/test.py
+++ code/do_not_change/general_utilities/test.py
@@ -6,7 +6,10 @@ while True:
 
 # ideas:
 
+# wrong default icon
+#
 
-# always ontop option or on/off Button
-# highlight on print option or on/off Button
+# always ontop option or on/off Button on left
+# highlight on print option or on/off Button on left
 # confimration proomtp on close or no allow close
+# option to turn on/off printing of commands and button on left
