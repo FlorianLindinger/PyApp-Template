c67337d2ebb3767502d5d7855c60e28dabb58430
Florian Lindinger <53214402+FlorianLindinger@users.noreply.github.com>
2025-11-20 22:23:54 +0100

    Aktualisieren von test.py

--- code/do_not_change/general_utilities/test.py
+++ code/do_not_change/general_utilities/test.py
@@ -20,9 +20,36 @@ while True:
 
 # prevent window from going too low to get back. apparently Windows makes it snap up
 
-
-
 # dragging away from max makes it scale instaed of snap to old size and drag
+
+#Option to print to file parallel and link to that file
+
+#Calculator option?
+
+#Option to open logged file for crash
+
+#Can I right click paste and copy?
+
+#Auto clickable links and paths?
+
+#Print of name in asci Option
+
+#Stop python crash and print and give options to restart? Open windows prompt for crash
+
+#----------------
+# interactions
+#--------------
+
+#Option to remember last state? Safe history? Relaod?
+
+#Maybe tabs for chat and for plots
+
+#Does progressbars work? Maybe build one in
+
+#Give option to change color of taskbar icon during runtime and before. tell user how to change icon and name during runtime and also color
+
+#any way of python to communicate with terminal. for like buttons.
+#Help button giving all commands
 
 # #=====================================
 # settings
@@ -40,6 +67,9 @@ while True:
 # end fix
 # #=====================================
 
+#Add as option to PyApp-Template
+
+#How does it abfangen commands that do terminal stuff
 
 # remove unneeded args
 
