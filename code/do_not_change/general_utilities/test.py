1599bde2f08e2320532879419a32e7869283125d
Florian.Lindinger <florianlindinger26@gmail.com>
2025-11-20 21:12:53 +0100

    included system tray file into main file

--- code/do_not_change/general_utilities/test.py
+++ code/do_not_change/general_utilities/test.py
@@ -12,17 +12,7 @@ while True:
 
 # mkae option to remove or add timestaps to all orints. make it removeber timesatap eve n if not tunred on
 
-# if maximized i cant move window
-
-# i cant snap to site like windows
-
-# make options for all buttons to appear or not and what defaults they have. maybe as file?
-
 # make when blue on hove it make the button icon white
-
-# maybe add option to remove tray minimizer 
-
-# option to disable print highlight
 
 # change maximize button icon when maximaize like Windows
 
@@ -30,9 +20,21 @@ while True:
 
 # prevent window from going too low to get back. apparently Windows makes it snap up
 
-# add color options
+
 
 # dragging away from max makes it scale instaed of snap to old size and drag
+
+# #=====================================
+# settings
+# #=====================================
+
+# add color options
+
+# option to disable print highlight
+
+# maybe add option to remove tray minimizer 
+
+# make options for all buttons to appear or not and what defaults they have. maybe as file?
 
 # #=====================================
 # end fix
