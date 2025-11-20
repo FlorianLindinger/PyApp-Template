afc347b700f917c20f25ba556f55969348be294d
Florian.Lindinger <florianlindinger26@gmail.com>
2025-11-20 20:10:12 +0100

    update todo; removed print ; tray minimize now remebers old position

--- code/do_not_change/general_utilities/test.py
+++ code/do_not_change/general_utilities/test.py
@@ -9,11 +9,21 @@ while True:
 
 # ideas:
 
-
-# open soruce code on click on title
 # make system tray to noremal remeber old pos
-#make it work on single click and also make if hihgligh blue as the other buttons
 
 # update docstirng
 
 # mkae option to remove or add timestaps to all orints. make it removeber timesatap eve n if not tunred on
+
+# if maximized i cant move window
+
+# i cant snap to site like windows
+
+# no resize moving edge on window
+
+# terminal minimitze does not rember pos of maximize
+
+
+# make options for all buttons to appear or not and what defaults they have. maybe as file?
+
+# remove unneeded args
