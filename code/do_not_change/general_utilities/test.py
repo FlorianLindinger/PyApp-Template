11be4b2fc7d649125d94e13a27f593d505a35c6d
Florian.Lindinger <florianlindinger26@gmail.com>
2025-11-20 20:13:11 +0100

    update todo

--- code/do_not_change/general_utilities/test.py
+++ code/do_not_change/general_utilities/test.py
@@ -6,12 +6,11 @@ while True:
     print(msg)
 
 
+# #=====================================
+#todo
+# #=====================================
 
-# ideas:
-
-# make system tray to noremal remeber old pos
-
-# update docstirng
+# no resize moving edge on window
 
 # mkae option to remove or add timestaps to all orints. make it removeber timesatap eve n if not tunred on
 
@@ -19,11 +18,13 @@ while True:
 
 # i cant snap to site like windows
 
-# no resize moving edge on window
-
-# terminal minimitze does not rember pos of maximize
-
-
 # make options for all buttons to appear or not and what defaults they have. maybe as file?
 
+# #=====================================
+# end fix
+# #=====================================
+
+
 # remove unneeded args
+
+# update docstirng
