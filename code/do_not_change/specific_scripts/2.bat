d8a2f5501c9bc5a18d38d8d64d2417d9fe16ebf4
Florian.Lindinger <florianlindinger26@gmail.com>
2026-02-01 03:30:45 +0100

    made generated shortcuts call now launcher scripts for shorter shortcuts "target" and possiblity to change stuff in non-user settings wiithout having to redo shortucts on user side; moved start_program again out of 1.bat

--- code/do_not_change/specific_scripts/2.bat
+++ code/do_not_change/specific_scripts/2.bat
@@ -1,3 +1,68 @@
-@echo off
+:: ========================
+:: --- Code Description ---
+:: ========================
 
-call "..\general_utilities\batch_file\run_batch_with_file_output_and_no_terminal.bat" %*
+:: ====================================
+:: --- Setup, Variables, and Checks ---
+:: ====================================
+
+:: turn off printing of commands & make variables local & allow for delayed expansion (needed for imported settings with ! inside)
+@echo off & setlocal EnableDelayedExpansion
+
+:: define local variables (with relative paths being relative to this file)
+set "settings_path=..\..\non-user_settings.ini"
+
+:: import settings from settings_path:
+for /F "tokens=1,2 delims==" %%A IN ('findstr "^" "%settings_path%"') DO ( set "%%A=%%B" )
+
+:: check if needed variables are defined in settings_path
+if "%process_id_file_path%"=="" (
+	echo [Error] process_id_file_path not defined in "%settings_path%". Aborting. Press any key to exit.
+	pause > nul
+	exit /b 2
+)
+if "%log_path%"=="" (
+	echo [Error] log_path not defined in "%settings_path%". Aborting. Press any key to exit.
+	pause > nul
+	exit /b 2
+)
+
+:: convert the path settings that are relative to settings file to absolute paths:
+for %%I IN ("%settings_path%") DO set "settings_dir=%%~dpI"
+cd /d "%settings_dir%"
+call :set_abs_path "%log_path%" "log_path"
+call :set_abs_path "%process_id_file_path%" "process_id_file_path"
+cd /d "%current_file_path%"
+
+:: ======================
+:: --- Code Execution ---
+:: ======================
+
+call "..\general_utilities\batch_file\run_batch_with_file_output_and_no_terminal.bat" "start_program.bat" "%log_path%" "%process_id_file_path%"
+
+EXIT /b
+
+:: ====================
+:: ==== Functions: ====
+:: ====================
+
+::::::::::::::::::::::::::::::::::::::::::::::::
+:: function that converts relative (to current working directory) path {arg1} to absolute and sets it to variable {arg2}. Works for empty path {arg1} which then sets the current working directory to variable {arg2}. Raises error if {arg2} is missing:
+:: Usage:
+::    call :set_abs_path "%some_path%" "some_path"
+::::::::::::::::::::::::::::::::::::::::::::::::
+:set_abs_path
+    if "%~2"=="" (
+        echo [Error] Second argument is missing for :set_abs_path function in "%~f0". (First argument was "%~1"^). 
+        echo Aborting. Press any key to exit.
+        pause > nul
+        exit /b 1
+    )
+    if "%~1"=="" (
+        set "%~2=%CD%"
+    ) else (
+	    set "%~2=%~f1"
+    )
+goto :EOF
+
+:: =================================================
--- code/do_not_change/specific_scripts/3.bat
+++ code/do_not_change/specific_scripts/3.bat
@@ -1,3 +1,62 @@
-@echo off
+:: ========================
+:: --- Code Description ---
+:: ========================
 
-call "..\general_utilities\kill_process_with_id.bat" %*
+:: ====================================
+:: --- Setup, Variables, and Checks ---
+:: ====================================
+
+:: turn off printing of commands & make variables local & allow for delayed expansion (needed for imported settings with ! inside)
+@echo off & setlocal EnableDelayedExpansion
+
+:: define local variables (with relative paths being relative to this file)
+set "settings_path=..\..\non-user_settings.ini"
+
+:: import settings from settings_path:
+for /F "tokens=1,2 delims==" %%A IN ('findstr "^" "%settings_path%"') DO ( set "%%A=%%B" )
+
+:: check if needed variables are defined in settings_path
+if "%process_id_file_path%"=="" (
+	echo [Error] process_id_file_path not defined in "%settings_path%". Aborting. Press any key to exit.
+	pause > nul
+	exit /b 2
+)
+
+:: convert the path settings that are relative to settings file to absolute paths:
+for %%I IN ("%settings_path%") DO set "settings_dir=%%~dpI"
+cd /d "%settings_dir%"
+call :set_abs_path "%process_id_file_path%" "process_id_file_path"
+cd /d "%current_file_path%"
+
+:: ======================
+:: --- Code Execution ---
+:: ======================
+
+call "..\general_utilities\kill_process_with_id.bat" "%process_id_file_path%"
+
+EXIT /b
+
+:: ====================
+:: ==== Functions: ====
+:: ====================
+
+::::::::::::::::::::::::::::::::::::::::::::::::
+:: function that converts relative (to current working directory) path {arg1} to absolute and sets it to variable {arg2}. Works for empty path {arg1} which then sets the current working directory to variable {arg2}. Raises error if {arg2} is missing:
+:: Usage:
+::    call :set_abs_path "%some_path%" "some_path"
+::::::::::::::::::::::::::::::::::::::::::::::::
+:set_abs_path
+    if "%~2"=="" (
+        echo [Error] Second argument is missing for :set_abs_path function in "%~f0". (First argument was "%~1"^). 
+        echo Aborting. Press any key to exit.
+        pause > nul
+        exit /b 1
+    )
+    if "%~1"=="" (
+        set "%~2=%CD%"
+    ) else (
+	    set "%~2=%~f1"
+    )
+goto :EOF
+
+:: =================================================
