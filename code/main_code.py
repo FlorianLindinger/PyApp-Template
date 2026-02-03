f899efd79f0739a39bbe9d98d05d365a159969ac
Florian.Lindinger <florianlindinger26@gmail.com>
2026-02-03 12:11:54 +0100

    moved over pyqt5 code from pyBoson app for developement

--- code/main_code.py
+++ code/main_code.py
@@ -1,13 +1,3 @@
-# ==============================================================================
-# Add code at the bottom that runs with the start of the program.
-# ==============================================================================
-# Optional: Imports and converts user variables (e.g., name: value) in settings.yaml (access value via dictionary: s["name"]). Alternatively use settings.py directly.
-from settings import s # <-needs pyyaml package # noqa isort: skip type: ignore fmt: off
-# ==============================================================================
+from pyqt5_helpers import Q_start, Q_tabs
 
-import time
-
-for i in range(50):
-    print(i)
-    time.sleep(0.1)
-
+Q_start(lambda: Q_tabs("test"))
