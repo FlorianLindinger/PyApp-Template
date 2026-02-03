dd7394dbc64b146d82042313a64e9adcae6fdf61
Florian.Lindinger <florianlindinger26@gmail.com>
2026-02-03 16:09:26 +0100

    WIP; not working sidebar_GUI

--- code/main_code.py
+++ code/main_code.py
@@ -1,3 +1,17 @@
-from pyqt5_helpers import Q_start, Q_tabs
+import traceback
 
-Q_start(lambda: Q_tabs("test"))
+try:
+    from pyqt5_helpers import *
+
+    Q_start()
+
+    widget = Q_sidebar_GUI()
+    
+    # widget.sidebar.add(QLabel("test"))
+
+    Q_end(widget)
+except SystemExit:
+    pass
+except:
+    print(traceback.format_exc())
+    input("Press enter to exit")
