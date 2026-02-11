fb36a29e70d89463abf07e5fd22e599e2b6b0729
Florian.Lindinger <florianlindinger26@gmail.com>
2026-02-11 23:09:56 +0100

    moved stuff to archive

---
+++ code/archive/run_create_shortcut_for_compiled.bat
@@ -0,0 +1,50 @@
+@echo off
+
+set "shortcut_location=..\..\..\test_qt.lnk"
+set "wdir="
+set "path_to_terminal_emulator_exe=%~dp0compiled\run.exe"
+set "icon_file=..\..\icons\icon.ico"
+set "use_qt_terminal=1"
+set "app_id=appid1"
+set "args_for_target="
+set "args_for_script=""test"""
+
+call py create_shortcut_for_compiled.py "%shortcut_location%" "%wdir%" "%path_to_terminal_emulator_exe%" "%icon_file%" "%use_qt_terminal%" "%app_id%" "%args_for_target%" "%args_for_script%"
+
+set "shortcut_location=..\..\..\test_windows.lnk"
+set "use_qt_terminal=0"
+set "app_id=appid2"
+
+call py create_shortcut_for_compiled.py "%shortcut_location%" "%wdir%" "%path_to_terminal_emulator_exe%" "%icon_file%" "%use_qt_terminal%" "%app_id%" "%args_for_target%" "%args_for_script%"
+
+set "shortcut_location=..\..\..\test_global_python_qt.lnk"
+set "use_qt_terminal=1"
+set "app_id=appid3"
+set "path_to_terminal_emulator_exe=""%SystemRoot%\py.exe"""
+set "args_for_target=-3 ""%~dp0pyside6_terminal.py"""
+
+call py create_shortcut_for_compiled.py "%shortcut_location%" "%wdir%" "%path_to_terminal_emulator_exe%" "%icon_file%" "%use_qt_terminal%" "%app_id%" "%args_for_target%" "%args_for_script%"
+
+set "shortcut_location=..\..\..\test_runtime_python_qt1.lnk"
+set "use_qt_terminal=1"
+set "app_id=appid5"
+set "path_to_terminal_emulator_exe=""%~dp0..\python_runtime\python.exe"""
+set "args_for_target=""%~dp0test.py"""
+
+call py create_shortcut_for_compiled.py "%shortcut_location%" "%wdir%" "%path_to_terminal_emulator_exe%" "%icon_file%" "%use_qt_terminal%" "%app_id%" "%args_for_target%" "%args_for_script%"
+
+set "shortcut_location=..\..\..\test_runtime_python_qt2.lnk"
+set "use_qt_terminal=1"
+set "app_id=appid6"
+set "path_to_terminal_emulator_exe=""%~dp0..\python_runtime\python.exe"""
+set "args_for_target=""%~dp0test.py"""
+
+call py create_shortcut_for_compiled.py "%shortcut_location%" "%wdir%" "%path_to_terminal_emulator_exe%" "%icon_file%" "%use_qt_terminal%" "%app_id%" "%args_for_target%" "%args_for_script%"
+
+set "shortcut_location=..\..\..\test_cmd_qt.lnk"
+set "app_id=appid4"
+set "path_to_terminal_emulator_exe=cmd.exe"
+set "args_for_target=/k ""py -3 %~dp0pyside6_terminal.py"
+set "args_for_script=""test"""""
+
+call py create_shortcut_for_compiled.py "%shortcut_location%" "%wdir%" "%path_to_terminal_emulator_exe%" "%icon_file%" "%use_qt_terminal%" "%app_id%" "%args_for_target%" "%args_for_script%"
