3a995023c2d776465455b0c16e7bc34171a1f395
Florian.Lindinger <florianlindinger26@gmail.com>
2026-03-01 22:46:23 +0100

    added rerouting .py scripts for short paths

---
+++ code/do_not_change/q_T.py
@@ -0,0 +1,14 @@
+# This script is just a shortcut to the actual script (at relative_path_to_actual_script) to have a shorter path in Windows shortcuts
+
+relative_path_to_actual_script = r"specific_scripts\stop_program.py"
+
+import os
+import subprocess
+import sys
+
+script_path = os.path.join(os.path.dirname(__file__), relative_path_to_actual_script)
+python_exe = sys.executable
+
+result = subprocess.run([python_exe, script_path])  # noqa:S603
+
+sys.exit(result.returncode)
