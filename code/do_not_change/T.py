6b65cfab4686e78cd85809669d468cdfdc43aeea
Florian.Lindinger <florianlindinger26@gmail.com>
2026-04-05 21:12:03 +0200

    moved stuff around

--- code/do_not_change/T.py
+++ code/do_not_change/T.py
@@ -1,17 +1,19 @@
 # This script is just a shortcut to the actual script (at relative_path_to_actual_script) to have a shorter path in Windows shortcuts
 
+relative_path_to_actual_script = "specific_scripts\\start_program.py"
+
+###################
+
 import os
 import subprocess
 import sys
 
-relative_path_to_actual_script = r"specific_scripts\start_program.py"
-args = [sys.argv[1], "1"]  # [appid,create_terminal]
-
 ###################
 
-script_path = os.path.join(os.path.dirname(__file__), relative_path_to_actual_script)
-python_exe = sys.executable
+args = [sys.argv[1], "1"]  # [app_id,create_terminal] # create_terminal is "1" to create a terminal, "0" to not create a terminal
 
-result = subprocess.run([python_exe, script_path, *args])  # noqa:S603
+script_path = os.path.join(os.path.dirname(__file__), relative_path_to_actual_script)
+
+result = subprocess.run([sys.executable, script_path, *args])  # noqa:S603
 
 sys.exit(result.returncode)
