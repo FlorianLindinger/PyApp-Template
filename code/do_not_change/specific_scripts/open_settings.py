8546b0a6006b26dfa5e8381fcc07f978ce7ef950
Florian.Lindinger <florianlindinger26@gmail.com>
2026-04-05 21:51:47 +0200

    not sure why was not commited before

--- code/do_not_change/specific_scripts/open_settings.py
+++ code/do_not_change/specific_scripts/open_settings.py
@@ -1,14 +1,33 @@
-import pathlib
+# ==========================================================================
+#   Local Variables
+# ==========================================================================
 
-import launcher_utilities as utils
+rel_path_to_settings_ini = "..\\..\\non-user_settings.ini"
 
+# ==========================================================================
+# package imports
 
-def main():
-    script_dir = pathlib.Path(__file__).parent.resolve()
-    settings_path = (script_dir / "../../non-user_settings.ini").resolve()
-    settings = utils.get_settings(settings_path)
-    utils.open_settings(settings_path, settings)
+import os
+import sys
 
+# ==========================================================================
+# move to folder of this file and add project root to sys.path for package imports
 
-if __name__ == "__main__":
-    main()
+file_path = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.normpath(file_path + "\\..\\..")
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
+os.chdir(file_path)
+
+# ==========================================================================
+# import from helper_functions
+
+from do_not_change.specific_scripts.helper_functions import get_settings, make_abs_path_relative_to_file, open_in_editor
+
+# ==========================================================================
+# code execution
+
+settings = get_settings(rel_path_to_settings_ini)
+user_settings_path = make_abs_path_relative_to_file(settings["user_settings_path"], rel_path_to_settings_ini)
+
+open_in_editor(user_settings_path)
