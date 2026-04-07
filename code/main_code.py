d9e3c1f0942ea09b9c0ef478871935c4432718d8
Florian.Lindinger <florianlindinger26@gmail.com>
2026-04-07 15:43:28 +0200

    update test script

--- code/main_code.py
+++ code/main_code.py
@@ -1,176 +1,76 @@
 import time
 
+# import settings
+
+for i in range(50):
+    print(i)
+    time.sleep(0.05)
 while True:
-    msg= input()
-    if msg=="error":
+    msg = input()
+    if msg == "error":
         raise Exception("error")
-    elif msg=="crash1":
+    elif msg == "crash1":
         raise RuntimeError("intentional crash test")
-    elif msg=="crash2":
+    elif msg == "crash2":
         import os
+
         os._exit(1)
-    elif msg=="crash3":
+    elif msg == "crash3":
         import os
+
         os.abort()
-    elif msg=="crash4":
+    elif msg == "crash4":
         import ctypes
+
         ctypes.string_at(0)  # dereference NULL -> segfault on most platforms
-    elif msg=="crash5":
+    elif msg == "crash5":
         a = []
         while True:
             a.append(b"x" * 10_000_000)
-    elif msg=="exit":
+    elif msg == "exit":
         break
-    elif msg=="sleep":
+    elif msg == "sleep":
         time.sleep(4)
         print(msg)
-    elif msg=="np":
+    elif msg in ["np","numpy"]:
         import numpy
-    
-    elif msg=="tv":
+
+    elif msg == "tv":
         print('Terminal_window.toggle_button_visible_state("stop")')
-    elif msg=="te":
+    elif msg == "te":
         print('Terminal_window.toggle_button_clickable_state("stop")')
     else:
         print(msg)
 
 
-# import sys
-# import time
 
-# import numpy as np
+import sys
 
-# for i in range(100):
-#     print(i)
-#     time.sleep(0.02)
-    
-# import os
-# print(os.getcwd())
-
-    
-# asd
-
-# test2.py script finishes naturally
-# sys.exit(0)
+sys.exit()
 
 
+import sys
 
+from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import QApplication, QMainWindow
 
-# import sys
-# import ctypes
-from PyQt5.QtWidgets import QApplication, QMainWindow
-
-# Tell Windows to use this AppID for this process
-# myAppID = "test2"
-# ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myAppID)
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        # self.setWindowTitle("My Custom App")
-        # self.resize(400, 300)
-        
-        # Optional: Set a specific window icon here if you want
-        # self.setWindowIcon(QIcon("icon.ico")) 
 
-if __name__ == '__main__':
+        if sys.argv[1] != "":
+            self.setWindowIcon(QIcon(sys.argv[1]))
+        if sys.argv[2] != "":
+            self.setWindowTitle(sys.argv[2])
+
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
-    
-    # 3. Important: If you use a custom icon, set it for the app as well
-    # app.setWindowIcon(QIcon("icon.ico"))
-    
+
     window = MainWindow()
     window.show()
-    sys.exit(app.exec_())
-
-# import time
-# import sys
-# import ctypes
-
-# ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("test2")
-
-# for i in range(50):
-#     print(i)
-#     time.sleep(0.1)
-
-# input("Press Enter to exit...")
-# sys.exit()
-
-# # app/app.py
-# import sys
-# # import ctypes
-# import traceback
-
-# # APPID = "test3"
-
-# # if sys.platform.startswith("win"):
-# #     ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(APPID)
-
-# try:
-#     from PyQt5.QtWidgets import QApplication, QMainWindow
-
-#     def main():
-#         app = QApplication(sys.argv)
-#         win = QMainWindow()
-#         win.setWindowTitle("My PyQt5 App")
-#         win.resize(900, 600)
-#         win.show()
-#         sys.exit(app.exec_())
-
-#     if __name__ == "__main__":
-#         main()
-
-# except Exception as e:
-#     print(traceback.format_exc())
-#     input("Press Enter to exit...")
-#     sys.exit(1)
-    
-    
-    
-    
-
-
-# import sys
-# import traceback
-# import os
-
-# # def set_appusermodel_id(appid: str) -> None:
-# #     if sys.platform.startswith("win"):
-# #         import ctypes
-# #         ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(appid)
-
-# # set_appusermodel_id("test2")  # must match the launcher’s ID
-
-# # from PyQt5.QtWidgets import QApplication
-# # app = QApplication(sys.argv)
-
-# # input("Press enter to exit")
-
-# # print(os.getcwd())
-# # os.chdir(os.path.dirname(os.path.abspath(__file__)))
-# # print(os.getcwd())
-
-# try:
-#     from GUI_code import pyqt5_helpers
-
-#     pyqt5_helpers.Q_start()
-
-#     widget = pyqt5_helpers.Q_sidebar_GUI()
-
-#     widget.sidebar.add(pyqt5_helpers.QLabel("test"))
-#     widget.sidebar.add(pyqt5_helpers.Q_file_path(label="test"))
-
-#     # widget.sidebar.add(QLabel("test"))
-
-
-#     pyqt5_helpers.Q_end(widget)
-# except SystemExit:
-#     pass
-# except:
-#     print(traceback.format_exc())
-#     input("Press enter to exit")
-
-# # input("Press enter to exit")
+    sys.exit(app.exec())
 
 
 # # # ==============================================================================
