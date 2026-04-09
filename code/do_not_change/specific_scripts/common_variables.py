6b8e51944b0de85ce49e292f07fdf757e8410cd0
Florian.Lindinger <florianlindinger26@gmail.com>
2026-04-09 22:41:04 +0200

    brok up common_code and moved stuff to script becasue almost nothing was shared code

--- code/do_not_change/specific_scripts/common_variables.py
+++ code/do_not_change/specific_scripts/common_variables.py
@@ -2,27 +2,33 @@ import os
 import sys
 
 #############################
-# local variables (might also be imported by callers)
+# variables
 
-file_dir = os.path.dirname(os.path.abspath(__file__)) + "\\"
+make_abs = lambda x: os.path.normpath(x)  # noqa
+get_dir = lambda x: os.path.dirname(x)  # noqa
 
-developer_settings_path = os.path.normpath(file_dir + "..\\..\\developer_settings.py")
+file_dir = get_dir(make_abs(__file__)) + "\\"
 
-portable_python_installer_path = os.path.normpath(file_dir + "..\\general_scripts\\create_portable_python.bat")
-portable_venv_creator_path = os.path.normpath(file_dir + "..\\general_scripts\\create_portable_venv.bat")
+developer_settings_path = make_abs(file_dir + "..\\..\\developer_settings.py")
 
-py_env_folder_path = os.path.normpath(file_dir + "..\\..\\py_env")
+portable_python_installer_path = make_abs(file_dir + "..\\general_scripts\\create_portable_python.bat")
+portable_venv_creator_path = make_abs(file_dir + "..\\general_scripts\\create_portable_venv.bat")
 
-backend_python_exe_path = os.path.normpath(file_dir + "..\\P\\P.exe")
+py_env_folder_path = make_abs(file_dir + "..\\..\\py_env")
+
+backend_python_exe_path = make_abs(file_dir + "..\\P\\P.exe")
 
 script_wrapper_path = file_dir + "script_wrapper.py"
 
 #############################
-# process local variables
+# process variables
 
-sys.path.insert(0, os.path.dirname(developer_settings_path))
-import developer_settings
+sys.path.insert(0, get_dir(developer_settings_path))
+import developer_settings # noqa
 
 python_dist_path = py_env_folder_path + "\\py_dist"
 venv_dir_path = py_env_folder_path + "\\virt_env"
 venv_exe_path = venv_dir_path + "\\Portable_Scripts\\python.bat"
+
+python_exe_path = make_abs(python_dist_path + "\\python.exe")
+relative_venv_to_python_dist = os.path.relpath(python_dist_path, get_dir(venv_dir_path))
--- code/do_not_change/specific_scripts/open_settings.py
+++ code/do_not_change/specific_scripts/open_settings.py
@@ -2,7 +2,10 @@
 # package imports
 
 import os
+import shutil
+import subprocess
 import sys
+import traceback
 
 # ==========================================================================
 # import from common_code_and_variables.py
@@ -12,14 +15,34 @@ project_root = os.path.normpath(file_path + "\\..\\..")
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
 
-from do_not_change.specific_scripts.common_code import (
-    make_abs_path_relative_to_file,
-    open_in_editor,
-)
 from do_not_change.specific_scripts.common_variables import (
     developer_settings,
     developer_settings_path,
 )
+
+# ==========================================================================
+# needed functions
+
+
+def open_in_editor(path):
+    try:
+        if not os.path.exists(path):
+            print(f"Could not find file at path: {path}")
+        vscode_exe_path = shutil.which("code")
+        if vscode_exe_path is not None:
+            subprocess.Popen([vscode_exe_path, path])  # noqa:S603
+        else:
+            # Fallback
+            subprocess.Popen(["notepad.exe", path])  # noqa:S603
+    except Exception as _e:
+        print(traceback.format_exc())
+
+def make_abs_path_relative_to_file(path, file):
+    """makes a path absolute if relative with respect to the file (as if the file defined it)"""
+    if not os.path.isabs(path):
+        return os.path.normpath(os.path.dirname(file) + "\\" + path)
+    else:
+        return path
 
 # ==========================================================================
 # code execution
