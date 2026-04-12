eb1ddba9f5b417ba8b59b8af6b3e4e527e8c37b7
Florian.Lindinger <florianlindinger26@gmail.com>
2026-04-12 21:46:49 +0200

    tweaked debug launcher a little

--- code/do_not_change/terminal_emulator/test_compiled_terminal.bat
+++ code/do_not_change/terminal_emulator/test_compiled_terminal.bat
@@ -5,7 +5,7 @@
 compiled\run.exe "..\..\main_code.py" "..\..\py_env\virt_env\portable_Scripts\python.bat" "Test Title" "..\..\icons\icon.ico" "pyapp-template" "1" "0" "0" "0" "log.txt" ""
 
 echo.
-echo Press any key to reopen...
+echo Press any key to reopen
 pause > nul
 cls
 goto start
--- code/do_not_change/terminal_emulator/test_uncompiled_terminal.bat
+++ code/do_not_change/terminal_emulator/test_uncompiled_terminal.bat
@@ -5,6 +5,7 @@
 py terminal_emulator.py "..\..\main_code.py" "..\..\py_env\virt_env\portable_Scripts\python.bat" "Test Title" "..\..\icons\icon.ico" "pyapp-template" "1" "0" "0" "0" "log.txt" ""
 
 echo.
+echo Press any key to reopen
+pause > nul
 cls
-echo REOPENED TEST TERMINAL:
 goto start
