2112f4f4a34d583c3e2813db281ae2bb7ff55e9f
Florian.Lindinger <florianlindinger26@gmail.com>
2026-04-26 01:15:40 +0200

    improeved error handling

--- code/do_not_change/N.py
+++ code/do_not_change/N.py
@@ -1,33 +1,61 @@
-# This script is just a shortcut to the actual script (at relative_path_to_actual_script) to have a shorter path in Windows shortcuts
-
-relative_path_to_actual_script = "specific_scripts\\start_program.py"
-
-###################
+"""This script is just a shortcut to the actual script (at relative_path_to_actual_script) to have a shorter path in Windows shortcuts""" #noqa
 
 import os
-import runpy
-import sys
-
-###################
-
-script_path = os.path.join(os.path.dirname(__file__), relative_path_to_actual_script)
-
-sys.argv = [
-    script_path,
-    sys.argv[1],  # app id
-    "0",  # create_terminal: 1" = create, "0" = not
-] 
+import traceback
 
 try:
-    runpy.run_path(script_path, run_name="__main__")
-    exit_code = 0
-except SystemExit as e:
-    if isinstance(e.code, int):
-        exit_code = e.code
-    elif e.code is None:
-        exit_code = 0
-    else:
-        # e.code can be something else which is treated as error
-        exit_code = 1
+    relative_path_to_actual_script = "specific_scripts\\start_program.py"
 
-sys.exit(exit_code)
+    # ===============
+
+    import runpy
+    import sys
+
+    # ===============
+
+    script_path = os.path.join(os.path.dirname(__file__), relative_path_to_actual_script)
+
+    sys.argv = [
+        script_path,
+        sys.argv[1],  # app id
+        "0",  # create_terminal: 1" = create, "0" = not
+    ] 
+
+    # ===============
+
+    try:
+        runpy.run_path(script_path, run_name="__main__")
+        exit_code = 0
+    except SystemExit as e:
+        if isinstance(e.code, int):
+            exit_code = e.code
+        elif e.code is None:
+            exit_code = 0
+        else:
+            # e.code can be something else which is treated as error
+            exit_code = 1
+            
+    # ===============
+
+    if exit_code == 0:
+        sys.exit(0)
+    else:
+        print()
+        print()
+        print("=" * 20)
+        print(f'[Error] "{script_path}" returned with failure code: {exit_code}')
+        print("-" * 20)
+        input("[Error] Press enter to exit")
+        os._exit(1)  # instead of sys.exit(1) to prevent exception by script calling this script
+
+except Exception as e:
+    print()
+    print()
+    print("=" * 20)
+    print(f'[Error] Failure in running "{__file__}": {e}')
+    print("-" * 20)
+    print(traceback.format_exc())
+    print("=" * 20)
+    input("[Error (see above)] Press enter to exit")
+    os._exit(1)  # instead of sys.exit(1) to prevent exception by script calling this script
+
--- code/do_not_change/Q.py
+++ code/do_not_change/Q.py
@@ -1,19 +1,23 @@
-# This script is just a shortcut to the actual script (at relative_path_to_actual_script) to have a shorter path in Windows shortcuts
+"""This script is just a shortcut to the actual script (at relative_path_to_actual_script) to have a shorter path in Windows shortcuts""" #noqa
+
+import os
+import traceback
 
 try:
     relative_path_to_actual_script = "specific_scripts\\stop_program.py"
 
-    ###################
+    # ===============
 
-    import os
     import runpy
     import sys
 
-    ###################
+    # ===============
 
     script_path = os.path.join(os.path.dirname(__file__), relative_path_to_actual_script)
 
     sys.argv = [script_path]
+
+    # ===============
 
     try:
         runpy.run_path(script_path, run_name="__main__")
@@ -26,16 +30,27 @@ try:
         else:
             # e.code can be something else which is treated as error
             exit_code = 1
+            
+    # ===============
 
-    sys.exit(exit_code)
+    if exit_code == 0:
+        sys.exit(0)
+    else:
+        print()
+        print()
+        print("=" * 20)
+        print(f'[Error] "{script_path}" returned with failure code: {exit_code}')
+        print("-" * 20)
+        input("[Error] Press enter to exit")
+        os._exit(1)  # instead of sys.exit(1) to prevent exception by script calling this script
 
 except Exception as e:
-    import sys
-    import traceback
-
-    print(f"[Error] Failed before quitting of program with error: {e}:")
+    print()
+    print()
     print("=" * 20)
+    print(f'[Error] Failure in running "{__file__}": {e}')
+    print("-" * 20)
     print(traceback.format_exc())
     print("=" * 20)
-    input("Press enter to exit")
-    sys.exit(1)
+    input("[Error (see above)] Press enter to exit")
+    os._exit(1)  # instead of sys.exit(1) to prevent exception by script calling this script
