9a0c78048359591a6fa683b9d37c99b6e6eabea5
Florian.Lindinger <florianlindinger26@gmail.com>
2026-04-26 20:20:33 +0200

    better error handling for already closed and always remove file now.

--- code/do_not_change/specific_scripts/stop_program.py
+++ code/do_not_change/specific_scripts/stop_program.py
@@ -33,15 +33,23 @@ pid_path = make_abs_path_relative_to_file(process_id_file_path, developer_settin
 
 if not os.path.exists(pid_path):
     print(f"[Info] No PID file found at {pid_path}.")
+    print("This could mean it was already stopped via this script.")
+    input("Press enter to exit")
     sys.exit(0)
 
 try:
     with open(pid_path, encoding="utf-8") as f:
         pid = int(f.read().strip())
-
-    os.kill(pid, signal.SIGTERM)
-    print("[Success] Process stopped.")
-    time.sleep(1)
-    sys.exit(0)
+    os.remove(pid_path)
+    try:
+        os.kill(pid, signal.SIGTERM)
+        print("[Success] Process stopped.")
+        time.sleep(1)
+        sys.exit(0)
+    except PermissionError:
+        print("PermissionError when trying to kill process. This could mean the program is no longer running anyway.")
+        input("Press enter to exit.")
+        sys.exit(0)
+        
 except Exception as e:
     print_traceback(f"[Error] Failed to stop process: {e}", add_press_enter_to_exit=True)
