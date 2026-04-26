0756a74d0a203c30cdd4bc96253c16b04e0a5d72
Florian.Lindinger <florianlindinger26@gmail.com>
2026-04-26 16:56:46 +0200

    update error hadnling

--- code/do_not_change/specific_scripts/open_settings.py
+++ code/do_not_change/specific_scripts/open_settings.py
@@ -1,56 +1,69 @@
-# ==========================================================================
-# package imports
+try:
+    # ==========================================================================
+    # package imports
 
-import os
-import shutil
-import subprocess
-import sys
-import traceback
+    import os
+    import shutil
+    import subprocess
+    import sys
+    import traceback
 
-# ==========================================================================
-# import from common variables and developer settings
-from do_not_change.specific_scripts.common_variables import (
-    developer_settings_dir,
-    developer_settings_path,
-)
+    # ==========================================================================
+    # import from common variables and developer settings
+    import developer_settings
+    from do_not_change.specific_scripts.common_variables import developer_settings_path
 
-sys.path.insert(0, developer_settings_dir)
-import developer_settings
+    # ==========================================================================
+    # needed functions
 
-# ==========================================================================
-# needed functions
-
-
-def open_in_editor(path):
-    try:
+    def open_settings_in_editor(path):
         if not os.path.exists(path):
-            print(f"Could not find file at path: {path}")
+            print(f"[Error] Could not find settings file at path: {path}")
+            input("Press enter to exit.")
+            sys.exit(0)
         vscode_exe_path = shutil.which("code")
         if vscode_exe_path is not None:
             subprocess.Popen([vscode_exe_path, path])  # noqa:S603
         else:
             # Fallback
             subprocess.Popen(["notepad.exe", path])  # noqa:S603
-    except Exception as _e:
-        print(traceback.format_exc())
 
+    def make_abs_path_relative_to_file(path, file):
+        """makes a path absolute if relative with respect to the file (as if the file defined it)"""
+        if not os.path.isabs(path):
+            return os.path.normpath(os.path.dirname(file) + "\\" + path)
+        else:
+            return path
 
-def make_abs_path_relative_to_file(path, file):
-    """makes a path absolute if relative with respect to the file (as if the file defined it)"""
-    if not os.path.isabs(path):
-        return os.path.normpath(os.path.dirname(file) + "\\" + path)
+    # ==========================================================================
+    # code execution
+
+    if not hasattr(developer_settings, "user_settings_path"):
+        print("[Error] Settings file is not implemented in program.")
+        input("Press enter to exit.")
+        sys.exit(0)
     else:
-        return path
+        user_settings_path = make_abs_path_relative_to_file(
+            developer_settings.user_settings_path, developer_settings_path
+        )
 
+    try:
+        open_settings_in_editor(user_settings_path)
+    except Exception as e:
+        print(f'[Error] Failed to open settings file at "{user_settings_path}": {e}')
+        print("=" * 20)
+        print(traceback.format_exc())
+        print("=" * 20)
+        input("Press Enter to close this window...")
+        sys.exit(1)
 
-# ==========================================================================
-# code execution
-
-user_settings_path = make_abs_path_relative_to_file(developer_settings.user_settings_path, developer_settings_path)
-
-try:
-    open_in_editor(user_settings_path)
 except Exception as e:
-    print(f'[Error] Failed to open settings file at "{user_settings_path}": {e}')
+    import sys
+    import traceback
+
+    print(f"[Error] Failed to start open settings file script: {e}")
+    print("=" * 20)
+    print(traceback.format_exc())
+    print("=" * 20)
     input("Press Enter to close this window...")
     sys.exit(1)
