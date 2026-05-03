369dffd82c67463ba5ea26fd879b4eb34def54b9
Florian.Lindinger <florianlindinger26@gmail.com>
2026-05-03 19:56:59 +0200

    update WIP

--- code/main_code.py
+++ code/main_code.py
@@ -1,77 +1,82 @@
-# # import settings
-# import os
-# import sys
-# import time
+# ==============================
 
-# for i in range(5):
-#     print(i)
-#     time.sleep(0.05)
-# while True:
-#     msg = input()
-#     if msg == "error1":
-#         raise Exception("error")
-#     elif msg == "error2":
-#         raise RuntimeError("intentional crash test")
-#     elif msg == "crash1":
-#         import os
-
-#         os._exit(1)
-#     elif msg == "crash2":
-#         import os
-
-#         os.abort()
-#     elif msg == "crash3":
-#         import ctypes
-
-#         ctypes.string_at(0)  # dereference NULL -> segfault on most platforms
-#     elif msg == "crash4":
-#         a = []
-#         while True:
-#             a.append(b"x" * 10_000_000)
-#     elif msg == "exit":
-#         break
-#     elif msg == "sleep":
-#         time.sleep(4)
-#         print(msg)
-#     elif msg in ["np", "numpy"]:
-#         import numpy
-#         print("imported numpy")
-
-#     elif msg == "tv":
-#         print('Terminal_window.toggle_button_visible_state("stop")')
-#     elif msg == "te":
-#         print('Terminal_window.toggle_button_clickable_state("stop")')
-#     else:
-#         print(msg)
-
-
-# import sys
-
-# sys.exit()
-
-
+import os
 import sys
+import time
 
-from PySide6.QtGui import QIcon
-from PySide6.QtWidgets import QApplication, QMainWindow
+for i in range(5):
+    print(i)
+    time.sleep(0.05)
+while True:
+    msg = input()
+    if msg == "error1":
+        raise Exception("error")
+    elif msg == "error2":
+        raise RuntimeError("intentional crash test")
+    elif msg == "crash1":
+        import os
+
+        os._exit(1)
+    elif msg == "crash2":
+        import os
+
+        os.abort()
+    elif msg == "crash3":
+        import ctypes
+
+        ctypes.string_at(0)  # dereference NULL -> segfault on most platforms
+    elif msg == "crash4":
+        a = []
+        while True:
+            a.append(b"x" * 10_000_000)
+    elif msg == "exit":
+        break
+    elif msg == "sleep":
+        time.sleep(4)
+        print(msg)
+    elif msg in ["np", "numpy"]:
+        import numpy
+        print("imported numpy")
+
+    elif msg == "tv":
+        print('Terminal_window.toggle_button_visible_state("stop")')
+    elif msg == "te":
+        print('Terminal_window.toggle_button_clickable_state("stop")')
+    else:
+        print(msg)
+
+sys.exit()
+
+# ==============================
 
 
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
+# ==============================
 
-        if sys.argv[1] != "":
-            self.setWindowIcon(QIcon(sys.argv[1]))
-        if sys.argv[2] != "":
-            self.setWindowTitle(sys.argv[2])
+# import sys
+
+# from PySide6.QtGui import QIcon
+# from PySide6.QtWidgets import QApplication, QMainWindow
+
+# class MainWindow(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
+
+#         if len(sys.argv)>1 and sys.argv[1] != "":
+#             self.setWindowIcon(QIcon(sys.argv[1]))
+#         if len(sys.argv)>2 and sys.argv[2] != "":
+#             self.setWindowTitle(sys.argv[2])
 
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
+# if __name__ == "__main__":
+#     app = QApplication(sys.argv)
 
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec())
+#     window = MainWindow()
+#     window.show()
+#     sys.exit(app.exec())
+
+# ==============================
+
+
 
 
 # # # ==============================================================================
