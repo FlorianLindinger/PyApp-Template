c03a215d5b9b07c3381a8aea3967bac5d140450b
Florian.Lindinger <florianlindinger26@gmail.com>
2026-05-05 04:06:08 +0200

    udpate and moved around stuff

--- code/developer_settings.py
+++ code/developer_settings.py
@@ -29,42 +29,6 @@ use_fancy_terminal = True
 # =================================
 
 # -------------------------------------------------
-# Text shown before user input when input is echoed in the terminal.
-input_prepend: str | None = "> "
-# -------------------------------------------------
-# success = sys.exit(0) or sys.exit() or no exit line.
-close_on_success = True
-play_sound_on_success = False
-send_Windows_notification_on_success = True
-# ----
-# failure = normal failure exit codes (i.e. exit_code != 0), usually via "sys.exit(exit_code)" or raised error.
-close_on_failure = False
-play_sound_on_failure = False
-send_Windows_notification_on_failure = True
-# ----
-# crash of python interpreter is usually caused by code causing Windows to kill Python and won't be caught by try/except.
-close_on_python_interpreter_crash = False
-play_sound_on_python_interpreter_crash = True
-send_Windows_notification_on_python_interpreter_crash = True
-# -------------------------------------------------
-# Path of end-user settings file (file can be deleted)
-user_settings_path: str | None = "settings.py"  # set None to not use. Can also be not python file.
-# -------------------------------------------------
-# Decide what parts of vanilla full Python install you actually need (enabling all is ~90 MB, disabling all is ~47 MB):
-# ----
-#   Include Tkinter GUI library? Required for Tk-based GUIs or IDLE and used as default backend for matplotlib.pyplot. (~11 MB):
-install_tkinter = True
-#   Include Python's internal test suite (Lib/test)? Only needed for interpreter testing. (~31 MB):
-install_tests = False
-#   Include Python's "Tools" folder? Needed for: Language translation workflows/Python's code demos/old editors/old exe converters. (~1 MB, some installation time):
-install_tools = False
-# -------------------------------------------------
-# Name of Python code file and has to be in "code" folder.
-python_code_name = "main_code.py"
-# -------------------------------------------------
-script_after_python_interpreter_crash_name: str | None = "after_python_crash_code.py"
-# Script has to be in "code" folder. You can use the same name as python_code_name setting here. Note that the last argument will indicate that it was launched as a after-interpreter-crash script. You can test this in script via sys.argv[-1]=="crash". Set it to None to not launch anything after a interpreter crash.
-# -------------------------------------------------
 # Names of generated shortcuts:
 # (set to None to disable generation of specific shortcut)
 start_shortcut_name: str | None = f"{program_name}"  # start program with terminal
@@ -73,7 +37,36 @@ start_no_terminal_shortcut_name: str | None = f"{program_name} (no Terminal)"  #
 settings_shortcut_name: str | None = f"{program_name} - Settings"  # open settings file
 stop_shortcut_name: str | None = f"Stop {program_name}"  # stop all started programs
 # -------------------------------------------------
-use_faulthandler = True
+# success = sys.exit(0) or sys.exit() or no exit line.
+close_on_success = True
+play_sound_on_success: str | bool = False # False for no sound, True for default sound (notify.wav) or a path to a wav file relative to C:\Windows\Media\.
+send_Windows_notification_on_success = True
+# ----
+# failure = normal failure exit codes (i.e. exit_code != 0), usually via "sys.exit(exit_code)" or raised error.
+close_on_failure = False
+play_sound_on_failure: str | bool  = False # False for no sound, True for default sound ("Windows Critical Stop.wav") or a path to a wav file relative to C:\Windows\Media\.
+send_Windows_notification_on_failure = True
+# ----
+# crash of python interpreter is usually caused by code causing Windows to kill Python and won't be caught by try/except.
+close_on_python_interpreter_crash = False
+play_sound_on_python_interpreter_crash:str|bool  = True # False for no sound, True for default sound ("Windows Critical Stop.wav") or a path to a wav file relative to C:\Windows\Media\.
+send_Windows_notification_on_python_interpreter_crash = True
+# -------------------------------------------------
+# Path of end-user settings file (file can be deleted)
+user_settings_path: str | None = "settings.py"  # set None to not use. Can also be not python file.
+# -------------------------------------------------
+# Text shown before user input when input is echoed in the terminal.
+input_prepend: str | None = "> "
+# -------------------------------------------------
+# Name of Python code file and has to be in "code" folder.
+python_code_name = "main_code.py"
+# -------------------------------------------------
+script_after_python_interpreter_crash_name: str | None = "after_python_crash_code.py"
+# Script has to be in "code" folder. You can use the same name as python_code_name setting here. Note that the last argument will indicate that it was launched as a after-interpreter-crash script. You can test this in script via sys.argv[-1]=="crash". Set it to None to not launch anything after a interpreter crash.
+# -------------------------------------------------
+ship_backend_python_and_packages = True
+# -------------------------------------------------
+start_minimized = False
 # -------------------------------------------------
 
 # ==========================
@@ -128,6 +121,23 @@ button_settings: None | list[tuple[str, dict[str, bool]]] = [  # None = default 
     ("to_tray", {"clickable": True}),
     ("open_script", {"pinned": False}),
 ]
+# -------------------------------------------------
+
+# ==================================
+# ==== least important settings ====
+# ==================================
+
+# -------------------------------------------------
+use_faulthandler = True
+# -------------------------------------------------
+# Decide what parts of vanilla full Python install you actually need (enabling all is ~90 MB, disabling all is ~47 MB):
+# ----
+#   Include Tkinter GUI library? Required for Tk-based GUIs or IDLE and used as default backend for matplotlib.pyplot. (~11 MB):
+install_tkinter = True
+#   Include Python's internal test suite (Lib/test)? Only needed for interpreter testing. (~31 MB):
+install_tests = False
+#   Include Python's "Tools" folder? Needed for: Language translation workflows/Python's code demos/old editors/old exe converters. (~1 MB, some installation time):
+install_tools = False
 # -------------------------------------------------
 
 # =======================
