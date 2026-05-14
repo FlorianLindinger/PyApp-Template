dbf727e960c37c20710977f13cd73d60270d46c9
Florian.Lindinger <florianlindinger26@gmail.com>
2026-05-14 17:04:12 +0200

    renamed imported funcrtions

--- code/DONT_CHANGE/specific_scripts/dev_tools/install_packages_needed_in_python_files.py
+++ code/DONT_CHANGE/specific_scripts/dev_tools/install_packages_needed_in_python_files.py
@@ -5,10 +5,15 @@ import sys
 root_dir = os.path.dirname(__file__) + "\\..\\..\\.."
 sys.path.insert(0, root_dir)
 
-from DONT_CHANGE.specific_scripts.common_code import ensure_venv, input_success, install_requirements, print_traceback
+from DONT_CHANGE.specific_scripts.common_code import (
+    ensure_python_distro_and_venv,
+    input_success,
+    install_packages_from_file,
+    print_traceback,
+)
 from DONT_CHANGE.specific_scripts.common_variables import (
+    determined_needed_packages_output_file_path,
     excluded_folders_for_package_search,
-    needed_packages_output_file_path,
     python_scripts_dir,
 )
 from DONT_CHANGE.specific_scripts.dev_tools.dev_tools_common_code import (
@@ -17,13 +22,13 @@ from DONT_CHANGE.specific_scripts.dev_tools.dev_tools_common_code import (
 
 try:
     save_required_packages(
-        needed_packages_output_file_path,
+        determined_needed_packages_output_file_path,
         with_versions=False,
         folder=python_scripts_dir,
         ignored_folders=excluded_folders_for_package_search,
     )
-    ensure_venv()
-    install_requirements(needed_packages_output_file_path)
+    ensure_python_distro_and_venv()
+    install_packages_from_file(determined_needed_packages_output_file_path)
     print()
     input_success("[Success] Press enter to exit")
 except Exception as e:
