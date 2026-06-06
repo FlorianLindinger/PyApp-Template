0f9d82190752bb160dd47bfd8517c48d75bd27a4
Florian.Lindinger <florianlindinger26@gmail.com>
2026-06-07 00:31:33 +0200

    renamed backend_packages_requirements.txt

--- code/DONT_CHANGE/scripts/_common_variables.py
+++ code/DONT_CHANGE/scripts/_common_variables.py
@@ -20,7 +20,7 @@ backend_python_pth_file = (
 )  # must match Python version in install_backend_python.bat
 backend_python_zip_rel_path = "python312.zip"  # must match Python version in install_backend_python.bat
 backend_packages_dir = make_abs("..\\backend_packages")  # Update contents of backend_python_pth_file
-backend_package_requirements_file = make_abs("..\\backend_packages_requirements.txt")
+backend_package_requirements_file = make_abs("..\\backend_packages_list.txt")
 backend_files_to_delete_on_install = ["sqlite3.dll", "python.cat"]
 
 # frontend related
